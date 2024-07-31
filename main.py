9da3079f456e415943620d325386cf1b2fb52e22
R3dH00d1988 <26475361+R3dH00d1988@users.noreply.github.com>
2024-07-31 12:31:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,11 @@ load_dotenv()
 
 
 # Define templates
-/*
+'''
 templates = {"Case Study: Ebola": "config_ebola_case_study", "Demo 1": "config_demo1", "Demo 2": "config_demo2", "ai_assessment": "config", "MCQ Generator": "config_mcq_generator", "Debate an AI": "config_debate", "mSCT Tutor": "config_msct_tutor",
              "Find the Incorrect Fact": "config_incorrect_fact", "Alt Text Generator": "config_alt_text", "SOAP Notes Scoring": "config_soap", "Question Feedback Generator": "config_question_feedback", "Learning Obective Generator": "config_lo_generator", "Image Quiz": "config_image_quiz", "Zodiac Symbol": "config_zodiac"}
 
 selected_template = st.sidebar.selectbox("Select template", templates.keys())
-
 
 if "template" not in st.session_state or st.session_state.template != selected_template:
     st.session_state.template = selected_template
@@ -37,7 +36,7 @@ if "template" not in st.session_state or st.session_state.template != selected_t
     st.rerun()
 
 config_file = templates[selected_template]
-*/
+'''
 config_file = templates config_zodiac
 
 if config_file:
