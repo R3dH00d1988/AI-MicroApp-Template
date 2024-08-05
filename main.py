0dafe64512bbf30a93a045616544b6e745ddee66
R3dH00d1988 <26475361+R3dH00d1988@users.noreply.github.com>
2024-08-05 10:15:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 templates = {"Zodiac Symbol": "config_zodiac", "Case Study: Ebola": "config_ebola_case_study", "Demo 1": "config_demo1", "Demo 2": "config_demo2", "ai_assessment": "config", "MCQ Generator": "config_mcq_generator", "Debate an AI": "config_debate", "mSCT Tutor": "config_msct_tutor", "Find the Incorrect Fact": "config_incorrect_fact", "Alt Text Generator": "config_alt_text", "SOAP Notes Scoring": "config_soap", "Question Feedback Generator": "config_question_feedback", "Learning Obective Generator": "config_lo_generator", "Image Quiz": "config_image_quiz"}
 
-selected_template = st.sidebar.selectbox("Select template", templates.keys())
+# selected_template = st.sidebar.selectbox("Select template", templates.keys())
 
 if "template" not in st.session_state or st.session_state.template != selected_template:
     st.session_state.template = selected_template
